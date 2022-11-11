0906f51329b939208cbc04963c8c23514777a6a4
Henok <henok_206@yahoo.com>
2022-11-11 19:13:04 +0300

    initial comment

---
+++ fun1.py
@@ -0,0 +1,58 @@
+""""
+def my_function():
+    x = 10
+    print("value inside the function:", x)
+    x = 20
+    print("value outside the function:", x)
+
+
+my_function()
+"""
+
+
+def organic_function():
+    KLOC = 400
+    c = 2.5
+    a = 2.4
+    b = 1.05
+    d = 0.38
+    E = a*(KLOC)*b
+    td = c*(E)*d
+    print("the value of Effort is:", E)
+    print("the value of Time developent is:", td)
+    # the value of Effort is: 1050.0
+
+
+organic_function()
+
+
+def sem_function():
+    KLOC = 400
+    c = 2.5
+    a = 3.0
+    b = 1.12
+    d = 0.35
+    E = a*(KLOC)*b
+    td = c*(E)*d
+    print("the value of Effort is:", E)
+    print("the value of Time developent is:", td)
+    # the value of Effort is: 1120.0
+
+
+sem_function()
+
+
+def emb_function():
+    KLOC = 400
+    c = 2.5
+    a = 3.6
+    b = 1.20
+    d = 0.32
+    E = a*(KLOC)*b
+    td = c*(E)*d
+    print("the value of Effort is:", E)
+    print("the value of Time developent is:", td)
+    # the value of Effort is: 1200.0
+
+
+emb_function()
